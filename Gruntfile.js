621980b51c720c585f247788ce2066affab5a6b6
Harri Sarsa <harri.sarsa@appgyver.com>
2013-11-06 16:41:05 +0200

    added steroids-compile-sass task that checks for SASS files existence before trying to run grunt-contrib-sass task

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,42 +12,10 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>',
-      ],
-      options: {
-        jshintrc: '.jshintrc',
-      },
-    },
-
     // Before generating any new files, remove any previously-created files.
     clean: {
       tests: ['tmp'],
     },
-
-    // Configuration to be run (and then tested).
-    steroids_defaults: {
-      default_options: {
-        options: {
-        },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!',
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
-    },
-
     // Unit tests.
     nodeunit: {
       tests: ['test/*_test.js'],
@@ -65,7 +33,7 @@ module.exports = function(grunt) {
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'steroids_defaults', 'nodeunit']);
+  grunt.registerTask('test', ['clean', 'steroids-make', 'nodeunit']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
